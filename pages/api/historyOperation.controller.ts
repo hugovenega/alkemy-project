4d7671bce7966f6528dab9d846ed48226d8e2e92
Hugo Venega <hugo.venega.00@gmail.com>
2022-01-28 13:19:25 -0300

    Add history operation endpoint

---
+++ pages/api/historyOperation.controller.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function operationHistory(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'GET') {
+    return res.json({ message: 'Method not allowed' })
+  }
+
+  const getUser = await prisma.user.findUnique({
+    where: {
+      id: 1, //change for req.body.id
+    },
+  })
+
+  if (!getUser) {
+    return res.json({ message: 'username not found' })
+  }
+  const getUserOperations = await prisma.operation.findMany({
+    where: {
+      authorId: 1, //change for req.body.id
+    },
+    orderBy: {
+      Date: 'desc',
+    },
+    take: 10,
+  })
+  return res.status(200).json({ getUserOperations })
+}
